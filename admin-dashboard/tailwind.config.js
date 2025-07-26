91e7c62116841b8d05932853bd1828204a8f8d28
Job Boronsong <jboronsong@gmail.com>
2025-07-26 17:02:02 +0000

    commit

--- admin-dashboard/tailwind.config.js
+++ admin-dashboard/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // include all source files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
